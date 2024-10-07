1f7bfb8a515c45a04c6a26a2854652b0a4303e7d
Christopher Barrera <c.barrera@ensi.lan>
2024-10-07 16:47:49 +0200

    Ajout Déplacement de la capsule

--- Assets/Christopher/Scripts/ControllerThxYvan.cs
+++ Assets/Christopher/Scripts/ControllerThxYvan.cs
@@ -4,25 +4,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class ControllerThxYvan : MonoBehaviour
-{
+public class ControllerThxYvan : MonoBehaviour {
     [SerializeField] private float _xPower = 30;
-    [SerializeField] private float _yPower = 100;
+    [SerializeField] private float _zPower = 100;
     private Rigidbody _rb;
 
-    private void Awake()
-    {
+    private void Awake() {
         _rb = GetComponent<Rigidbody>();
     }
 
-    public void Deplacement(InputAction.CallbackContext ctx)
-    {
+    public void Deplacement(InputAction.CallbackContext ctx) {
         Debug.Log(ctx.ReadValue<Vector3>());
 
         Vector3 vec = ctx.ReadValue<Vector3>();
         vec.x = vec.x * _xPower;
-        vec.y = vec.y * _yPower;
-        vec.z = 0;
+        vec.z = vec.y * _zPower;
+        vec.y = 0;
         _rb.velocity = vec;
     }
 }
